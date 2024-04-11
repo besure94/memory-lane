fe0dcafee5942b4c9bba9e94266ed9452568dbb5
Brian Scherner <brianscherner@gmail.com>
2024-04-11 11:09:47 -0700

    Create Header for application.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>Memory Lane</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Header;
