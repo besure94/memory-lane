91c5a9ef57e6114e44c784f391ae3d7f9e2b145a
Brian Scherner <brianscherner@gmail.com>
2024-04-11 15:00:26 -0700

    Refactor application to use function component instead of class to handle state.

--- src/components/MemoryControl.js
+++ src/components/MemoryControl.js
@@ -1,48 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import NewMemoryForm from "./NewMemoryForm";
 import MemoryList from "./MemoryList";
 
-class MemoryControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formVisibleOnPage: false,
-      mainMemoryList: []
-    };
+function MemoryControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainMemoryList, setMainMemoryList] = useState([]);
+
+  const handleClick = () => {
+    setFormVisibleOnPage(!formVisibleOnPage);
   }
 
-  handleClick = () => {
-    this.setState(prevState => ({formVisibleOnPage: !prevState.formVisibleOnPage}));
+  const handleCreatingNewMemory = (newMemory) => {
+    const newMainMemoryList = mainMemoryList.concat(newMemory);
+    setMainMemoryList(newMainMemoryList);
+    setFormVisibleOnPage(false);
   }
 
-  handleCreatingNewMemory = (newMemory) => {
-    const newMainMemoryList = this.state.mainMemoryList.concat(newMemory);
-    this.setState({
-      mainMemoryList: newMainMemoryList,
-      formVisibleOnPage: false
-    });
-  }
 
-  render() {
-    let currentlyVisibleState = null;
-    let buttonText = null;
+  let currentlyVisibleState = null;
+  let buttonText = null;
 
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewMemoryForm
-        onNewMemoryCreation={this.handleCreatingNewMemory}/>;
-      buttonText = "Home";
-    } else {
-      currentlyVisibleState = <MemoryList
-        memoryList={this.state.mainMemoryList}/>;
-      buttonText = "Create Memory";
-    }
-    return (
-      <React.Fragment>
-        {currentlyVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
-      </React.Fragment>
-    );
+  if (formVisibleOnPage) {
+    currentlyVisibleState = <NewMemoryForm
+      onNewMemoryCreation={handleCreatingNewMemory}/>;
+    buttonText = "Home";
+  } else {
+    currentlyVisibleState = <MemoryList
+      memoryList={mainMemoryList}/>;
+    buttonText = "Create Memory";
   }
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+      <button onClick={handleClick}>{buttonText}</button>
+    </React.Fragment>
+  );
 }
 
 export default MemoryControl;
