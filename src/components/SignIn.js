72b360beb49379a8341ca3501f7bf2566540ddfb
Brian Scherner <brianscherner@gmail.com>
2024-04-12 13:23:00 -0700

    Finish adding functionality to allow users to sign up, and store users in firestore.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "./../firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function SignIn() {
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+
   function doSignUp(event) {
     event.preventDefault();
     const email = event.target.email.value;
     const password = event.target.password.value;
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-
+        setSignUpSuccess(`Welcome, friend! You've successfully signed up as ${userCredential.user.email}!`);
       })
       .catch((error) => {
-
+        setSignUpSuccess(`There was an error signing up: ${error.message}`);
       });
   }
 
   return (
     <React.Fragment>
-      <h1>Sign In</h1>
+      <h1>Sign Up</h1>
+      {signUpSuccess}
       <form onSubmit={doSignUp}>
         <input
           type="text"
-          name="name"
-          placeholder="Email"/>
+          name="email"
+          placeholder="Email"
+          required/>
         <br/>
         <input
           type="password"
           name="password"
-          placeholder="Password"/>
+          placeholder="Password"
+          required/>
         <br/>
         <button type="submit">Sign Up</button>
       </form>
