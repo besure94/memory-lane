64907bc472b61a95684bee710f9796c64127ef46
Brian Scherner <brianscherner@gmail.com>
2024-04-16 11:56:45 -0700

    Refactor rendering of Edit and Delete buttons depending on current user.

--- src/components/MemoryDetail.js
+++ src/components/MemoryDetail.js
@@ -4,6 +4,8 @@ import { auth } from './../firebase.js';
 
 function MemoryDetail(props) {
   const { memory, onClickingDelete, onClickingEdit } = props;
+  const editButton = <button onClick={() => onClickingEdit(memory.id)}>Edit</button>;
+  const deleteButton = <button onClick={() => onClickingDelete(memory.id)}>Delete</button>;
 
   return (
     <React.Fragment>
@@ -15,17 +17,17 @@ function MemoryDetail(props) {
       <h3>{memory.description}</h3>
       <h3>Submitted by {memory.user}</h3>
 
-      {auth.currentUser.email != memory.user ? (
+      {auth.currentUser.email === memory.user ? (
         <React.Fragment>
-          <button style={{ display: "none" }} onClick={() => onClickingEdit(memory.id)}>Edit</button>
+          {editButton}
           <br/>
-          <button style={{ display: "none" }} onClick={() => onClickingDelete(memory.id)}>Delete</button>
+          {deleteButton}
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <button onClick={() => onClickingEdit(memory.id)}>Edit</button>
+          <button style={{display: "none"}}>{editButton}</button>
           <br/>
-          <button onClick={() => onClickingDelete(memory.id)}>Delete</button>
+          <button style={{display: "none"}}>{deleteButton}</button>
         </React.Fragment>
       )}
       <hr/>
