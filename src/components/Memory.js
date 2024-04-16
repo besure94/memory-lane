e8abdbabb47af75dee651cefa7e4bde633932f25
Brian Scherner <brianscherner@gmail.com>
2024-04-16 11:41:42 -0700

    Only render 'Edit' and 'Delete' buttons if the current user is same as the user who created the memory.

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -6,10 +6,6 @@ function Memory(props) {
     <React.Fragment>
       <div onClick={() => props.whenMemoryClicked(props.id)}>
         <h2>{props.name}</h2>
-        {/* <h3>When?</h3>
-        <h3>{props.when}</h3>
-        <h3>What?</h3>
-        <h3>{props.description}</h3> */}
         <h3>Submitted by {props.user}</h3>
         <hr/>
       </div>
@@ -19,8 +15,6 @@ function Memory(props) {
 
 Memory.propTypes = {
   name: PropTypes.string.isRequired,
-  // when: PropTypes.string.isRequired,
-  // description: PropTypes.string.isRequired,
   user: PropTypes.string.isRequired,
   id: PropTypes.string,
   whenMemoryClicked: PropTypes.func
--- src/components/MemoryDetail.js
+++ src/components/MemoryDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { auth } from './../firebase.js';
 
 function MemoryDetail(props) {
   const { memory, onClickingDelete, onClickingEdit } = props;
@@ -13,9 +14,20 @@ function MemoryDetail(props) {
       <h3>What?</h3>
       <h3>{memory.description}</h3>
       <h3>Submitted by {memory.user}</h3>
-      <button onClick={() => onClickingEdit(memory.id)}>Edit</button>
-      <br/>
-      <button onClick={() => onClickingDelete(memory.id)}>Delete</button>
+
+      {auth.currentUser.email != memory.user ? (
+        <React.Fragment>
+          <button style={{ display: "none" }} onClick={() => onClickingEdit(memory.id)}>Edit</button>
+          <br/>
+          <button style={{ display: "none" }} onClick={() => onClickingDelete(memory.id)}>Delete</button>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <button onClick={() => onClickingEdit(memory.id)}>Edit</button>
+          <br/>
+          <button onClick={() => onClickingDelete(memory.id)}>Delete</button>
+        </React.Fragment>
+      )}
       <hr/>
     </React.Fragment>
   );
