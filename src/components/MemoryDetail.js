b861d73c2cdae984b75c9ae676a1428e0b2057c3
Brian Scherner <brianscherner@gmail.com>
2024-04-19 11:01:25 -0700

    Center memory details and table in center of page.

--- src/components/MemoryDetail.js
+++ src/components/MemoryDetail.js
@@ -9,77 +9,81 @@ function MemoryDetail(props) {
 
   return (
     <React.Fragment>
-      <h2>Memory:</h2>
-      <p>{memory.name}</p>
-      <h2>Type:</h2>
-      <p>{memory.type}</p>
+      <div className="memory-detail">
+        <h2>Memory:</h2>
+        <p>{memory.name}</p>
+        <h2>Type:</h2>
+        <p>{memory.type}</p>
 
-      {memory.type === "Sensory" && (
-        <React.Fragment>
-          <h2>When?</h2>
-          <p>{memory.when}</p>
-          <table>
-            <thead>
-              <tr>
-                <th colspan="2"><h3 id="table-header">Senses</h3></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th>Touch</th>
-                <td>{memory.touch}</td>
-              </tr>
-              <tr>
-                <th>Sight</th>
-                <td>{memory.sight}</td>
-              </tr>
-              <tr>
-                <th>Sound</th>
-                <td>{memory.sound}</td>
-              </tr>
-              <tr>
-                <th>Smell</th>
-                <td>{memory.smell}</td>
-              </tr>
-              <tr>
-                <th>Taste</th>
-                <td>{memory.taste}</td>
-              </tr>
-            </tbody>
-          </table>
-        </React.Fragment>
-      )}
+        {memory.type === "Sensory" && (
+          <React.Fragment>
+            <h2>When?</h2>
+            <p>{memory.when}</p>
+            <div className="table-container">
+              <table>
+                <thead>
+                  <tr>
+                    <th colspan="2"><h3 id="table-header">Senses</h3></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Touch</th>
+                    <td>{memory.touch}</td>
+                  </tr>
+                  <tr>
+                    <th>Sight</th>
+                    <td>{memory.sight}</td>
+                  </tr>
+                  <tr>
+                    <th>Sound</th>
+                    <td>{memory.sound}</td>
+                  </tr>
+                  <tr>
+                    <th>Smell</th>
+                    <td>{memory.smell}</td>
+                  </tr>
+                  <tr>
+                    <th>Taste</th>
+                    <td>{memory.taste}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </React.Fragment>
+        )}
 
-      {memory.type === "Short Term" && (
-        <React.Fragment>
-          <h2>Details:</h2>
-          <p>{memory.shortTermDescription}</p>
-        </React.Fragment>
-      )}
+        {memory.type === "Short Term" && (
+          <React.Fragment>
+            <h2>Details:</h2>
+            <p>{memory.shortTermDescription}</p>
+          </React.Fragment>
+        )}
 
-      {memory.type === "Long Term" && (
-        <React.Fragment>
-          <h2>Description:</h2>
-          <p>{memory.longTermDescription}</p>
-        </React.Fragment>
-      )}
+        {memory.type === "Long Term" && (
+          <React.Fragment>
+            <h2>Description:</h2>
+            <p>{memory.longTermDescription}</p>
+          </React.Fragment>
+        )}
 
-      <p><em>Submitted by {memory.user} on {memory.date.toDateString()}</em></p>
+        <p><em>Submitted by {memory.user} on {memory.date.toDateString()}</em></p>
 
-      {auth.currentUser.email === memory.user ? (
-        <React.Fragment>
-          {editButton}
-          <br/>
-          {deleteButton}
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <button style={{display: "none"}}>{editButton}</button>
-          <br/>
-          <button style={{display: "none"}}>{deleteButton}</button>
-        </React.Fragment>
-      )}
-      <hr/>
+        {auth.currentUser.email === memory.user ? (
+          <React.Fragment>
+            {editButton}
+            <br/>
+            {deleteButton}
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <button style={{display: "none"}}>{editButton}</button>
+            <br/>
+            <button style={{display: "none"}}>{deleteButton}</button>
+          </React.Fragment>
+        )}
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
